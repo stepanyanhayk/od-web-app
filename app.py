17f60b44995890a002661a47fdbdcac2fa14382a
stepanyanhayk <haykstepanyan02@gmail.com>
2020-12-09 09:48:43 +0400

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.secret_key = "secret key"
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
+ALLOWED_EXTENSIONS = set(["jpg"])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -33,10 +33,6 @@ def upload_form():
 @app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
         if file.filename == '':
             flash('No file selected for uploading')
@@ -44,9 +40,15 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            object_detector_copy.run()
+            img = object_detector_copy.run()
+            img.save("static/detected_image.jpg")
             flash('File successfully uploaded')
-            return redirect('/')
+            return redirect("/detected")
         else:
-            flash('Allowed file types are png, jpg, jpeg, gif')
+            flash('Allowed file type so far is only jpg')
             return redirect(request.url)
+
+@app.route("/detected")
+def detected():
+    location = "static/detected_image.jpg"
+    return render_template("detected.html", location=location)
